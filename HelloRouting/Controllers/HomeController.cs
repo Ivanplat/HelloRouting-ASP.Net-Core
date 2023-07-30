40e2f00e8585ce25e40760fb1f351a2d0a2529db
Ivan Platonov <EventGraphDev@gmail.com>
2023-07-30 08:24:06 +0300

    Add project files.

---
+++ HelloRouting/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloRouting.Controllers
+{
+    public class HomeController
+    {
+        public string Index(string? id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return $"Привет из контроллера!";
+            }
+            else
+            {
+                return $"Привет! Это {id}.";
+            }
+        }
+    }
+}
---
+++ HelloRouting/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloRouting.Controllers
+{
+    [Route("Lk/[controller]/[action]")]
+    public class PersonController
+    {
+        [Route("[action]")]
+        public string Login()
+        {
+            return "temp@mail.ru";
+        }
+        [Route("[action]")]
+        public string Name()
+        {
+            return "Platonov. I. G.";
+        }
+        [Route("")]
+        [Route("[action]")]
+        public string Index()
+        {
+            return "Hello from personal account!";
+        }
+    }
+}
